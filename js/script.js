f458626388bfba8043445601e5274d87e8b0f382
I WAYAN GIRI M <site.phay@gmail.com>
2023-11-06 22:26:19 +0700

    javascript form

--- js/script.js
+++ js/script.js
@@ -12,3 +12,34 @@ const $ = (item) => {
   const $fill = (item, text) => {
     item.innerHTML = text;
   };
+
+  // Mendefinisikan nilai minimum untuk validasi form input
+  const validAge = 18;
+  const validWeight = 30;
+  const validHeight = 100;
+  
+  // Memilih semua elemen setiap kelas dari DOM dan menyimpannya ke dalam variabel
+  const numberInputs = $_(".number");
+  const errorMessages = $_(".error-message");
+  
+  // Melakukan iterasi setiap elemen input dalam "numberInputs"
+  numberInputs.forEach((input, i) => {
+    // Menambahkan event listener "input" pada setiap elemen input
+    input.addEventListener("input", () => {
+      // Validasi nilai input apakah memnuhi nilai minimum yang sudah ditentukan
+      if (!input.value) {
+        errorMessages[i].textContent = "Harap isi Berat Badan.";
+      } else if (i === 0 && input.value < validWeight) {
+        errorMessages[i].textContent =
+          "Tidak memenuhi nilai minimum yaitu " + validWeight + " kg";
+      } else if (i === 1 && input.value < validAge) {
+        errorMessages[i].textContent =
+          "Tidak memenuhi nilai minimum yaitu " + validAge + " tahun";
+      } else if (i === 2 && input.value < validHeight) {
+        errorMessages[i].textContent =
+          "Tidak memenuhi nilai minimum yaitu " + validHeight + " cm";
+      } else {
+        errorMessages[i].textContent = ""; // Clear error message if input is valid
+      }
+    });
+  });
